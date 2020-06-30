ce880cce753c628d291b2a53867f56271679a9d6
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-06-30 11:59:29 +0100

    implementing express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import 'dotenv/config';
+import express from 'express';
+
+const app = express();
 
 console.log('Hello World, simple nodemon loading app');
 
@@ -7,5 +10,9 @@ console.log(process.env.MY_SECRET);
 function sayHello() {
     return 'Hello World';
 };
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+})
 
 export default sayHello;
