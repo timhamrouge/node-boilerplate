fa67aaafc947d655c9538a3e9ad3cf2e789f5e7c
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-06-30 12:14:35 +0100

    moving PORT to env

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on port ${process.env.PORT}`)
 })
 
 export default sayHello;
