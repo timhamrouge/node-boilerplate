478f197c12d4537353e34bb727725b902a64d5cb
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-06-30 11:54:04 +0100

    implementing a spy to test the func

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,18 @@ import { spy } from 'sinon';
 import sayHello from './index.js';
 
 describe('sayHello', () => {
+  // simple test of the basic functionality
   it('should say "Hello World"', () => {
     expect(sayHello()).to.eql('Hello World');
+  })
+  // using spies to ensure the function has been called and is returning the correct thing
+  it('should call the function', () => {
+    const helloSpy = spy(sayHello);
+
+    helloSpy(sayHello);
+
+    expect(helloSpy.called).to.be.eql(true);
+    expect(helloSpy.returnValues[0]).to.be.eql('Hello World');
+
   })
 });
